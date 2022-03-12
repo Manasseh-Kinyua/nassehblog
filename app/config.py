451c5dacacfdaefa0d7d9f36061bfe89c8f2784c
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-12 08:25:22 +0300

    Add the api url to the request file to make the first api call

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
 
 
 
--- app/request.py
+++ app/request.py
@@ -1,1 +1,7 @@
 from app import app
+import urllib.request,json
+from .models import quotes
+
+Quotes = quotes.Quotes
+
+base_url = app.config['QOUTES_API_BASE_URL']
