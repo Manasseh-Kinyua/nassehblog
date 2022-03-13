1365441e9fef85da9cd0b55b3a52e9a37b35e7d5
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-13 13:14:21 +0300

    Add the functionality to display blog details on the home page

--- manage.py
+++ manage.py
@@ -1,17 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 #setting up configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///home/nasseh/Python-Flask/nassehblog/myblog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/nasseh/Python-Flask/nassehblog/myblog.db'
 db = SQLAlchemy(app)
 
 class PostedBlog(db.Model):
-    id = db.column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(50))
-    subtitle = db.Column(db.string(100))
+    subtitle = db.Column(db.String(100))
+    author = db.Column(db.String(50))
     date_of_post = db.Column(db.DateTime)
     blog_content = db.Column(db.Text)
 
@@ -25,10 +27,35 @@ def index():
     title = 'Home: Welcome to my blog'
     return render_template('index.html', title = title)
 
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = PostedBlog.query.filter_by(id=post_id).one()
+    return render_template('post.html', post=post)
+
+
 @app.route('/about')
 def about():
     title = 'Home: Welcome to my blog'
     return render_template('about.html', title = title)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    blog_content = request.form['blog_content']
+
+    post = PostedBlog(title=title, subtitle=subtitle, author=author, blog_content=blog_content, date_of_post=datetime.now())
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
 
 
 
